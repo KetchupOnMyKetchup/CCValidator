4528a6698e15ffe27195e6c2877f6a926cb95a50
Crystal Tenn <crtenn@microsoft.com>
2019-08-06 10:13:51 -0400

    add lses

--- CreditCardValidator.Droid/appcenter-post-build.sh
+++ CreditCardValidator.Droid/appcenter-post-build.sh
@@ -12,4 +12,22 @@ ls $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*
 echo "Source Directory/Xamarin UI test 226/tools"
 ls $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools
 
+echo "Source Directory/Droid"
+ls $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid
+
+echo "Source Directory/Droid.UITests"
+ls $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid.UITests
+
+echo "Source Directory/Droid.UITests/bin"
+ls $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid.UITests/bin
+
+echo "Source Directory/Droid.UITests/bin/Release"
+ls $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid.UITests/bin/Release
+
+echo "Source Directory/Droid/bin"
+ls $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid/bin
+
+echo "Source Directory/Droid/bin/Release"
+ls $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid/bin/Release
+
 appcenter test run uitest --app "Credit-Card-Team/Credit-Card-App" --devices "Credit-Card-Team/pixels" --app-path $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid/bin/Release/com.xamarin.example.creditcardvalidator.apk --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid.UITests/bin/Release --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools 
