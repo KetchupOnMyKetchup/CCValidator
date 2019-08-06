47578eae6bd15bdf8ebd466692ac1ea2abce6f42
Crystal Tenn <crtenn@microsoft.com>
2019-08-06 10:24:22 -0400

    add msbuild step

--- CreditCardValidator.Droid/appcenter-post-build.sh
+++ CreditCardValidator.Droid/appcenter-post-build.sh
@@ -1,5 +1,7 @@
 appcenter login --token "0469ed8ec3cfad43e95f1981f07ece88d543bc48"
 
+MSBuild.exe $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid.UITests/CreditCardValidator.Droid.UITests.csproj /property:Configuration=Release
+
 echo "Source Directory"
 ls $APPCENTER_SOURCE_DIRECTORY/
 
@@ -29,5 +31,8 @@ ls $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid/bin
 
 echo "Source Directory/Droid/bin/Release"
 ls $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid/bin/Release
+
+
+
 
 appcenter test run uitest --app "Credit-Card-Team/Credit-Card-App" --devices "Credit-Card-Team/pixels" --app-path $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid/bin/Release/com.xamarin.example.creditcardvalidator.apk --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/CreditCardValidator.Droid.UITests/bin/Release --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools 
